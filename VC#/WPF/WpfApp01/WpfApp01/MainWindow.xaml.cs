f47e02960a8612b345427bf4be615858c4db1eab
span.liu <spdroid@gmai.com>
2015-09-18 11:33:10 +0800

    [SP] first commit on VC# source.

---
+++ VC#/WPF/WpfApp01/WpfApp01/MainWindow.xaml.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Mail;  ///for mail
+using System.Net.Security;
+using System.Net;   ///TCP
+using System.Net.Sockets;   ///TCP
+using System.Threading; ///TCP
+
+using System.IO;
+///using System.Text;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            string temp;
+            temp = "MouseLeftButtonUp";
+            listBox1.Items.Add(temp);
+        }
+
+        private void button1_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            listBox1.Items.Add("MouseUp");
+        }
+
+        private void button1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            listBox1.Items.Add("MouseDown");
+        }
+
+        private void button1_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            listBox1.Items.Add("MouseRightButtonUp");
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Add("Click");
+            DateTime currdt = DateTime.Now;
+            listBox1.Items.Add("#$" + currdt.Ticks + "#In: " + ReadTextLine());
+            ///listBox1.Items.Add( ReadText() );
+            ///listBox1.Items.Add(ReadTextLine());
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxShow("button2 Click");
+
+            if (null == myInC.Find(textBox1.Text.ToString()))
+                myExC.AddLast(textBox1.Text);
+            else
+                myExC.Remove(myExC.Find(textBox1.Text.ToString()));
+
+
+            listBox2.Items.Clear();
+            int iLoop = 0;
+
+            while (iLoop < myInC.Count)
+            {
+                listBox2.Items.Add("INC:" + myInC.ElementAt<string>(iLoop));
+                iLoop++;
+            }
+            iLoop = 0;
+            while (iLoop < myExC.Count)
+            {
+                listBox2.Items.Add("EXC:" + myExC.ElementAt<string>(iLoop));
+                iLoop++;
+            }
+
+            textBox1.Clear();
+        }
+
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            ///ThreadedTcpSrvr server = new ThreadedTcpSrvr();
+            ListBoxShow("button3 Click");
+
+            if (null == myInC.Find(textBox1.Text.ToString()))
+                myInC.AddLast(textBox1.Text);
+            else
+                myInC.Remove(myInC.Find(textBox1.Text.ToString()));
+
+
+            listBox2.Items.Clear();
+            int iLoop = 0;
+
+            while(iLoop<myInC.Count)
+            {
+                listBox2.Items.Add("INC:" + myInC.ElementAt<string>(iLoop) );
+                iLoop++;
+            }
+            iLoop = 0;
+            while (iLoop < myExC.Count)
+            {
+                listBox2.Items.Add("EXC:" + myExC.ElementAt<string>(iLoop));
+                iLoop++;
+            }
+
+            textBox1.Clear();
+        }
+
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            myFile rawFile;
+            string sTmp;
+            
+            ListBoxShow("Load!!");
+
+            rawFile = new myFile("\\mcelog.raw");
+
+            while (!rawFile.IsEOF())
+            {
+                sTmp = rawFile.ReadNextLine();
+                ///ListBoxShow(ReadTextLine("\\mcelog.raw"));
+                if( 0 > sTmp.IndexOf(textBox1.Text.ToString()) )
+                    ListBoxShow(sTmp);
+            }
+
+            rawFile.myDestroy();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ///listBox1.Items.Add("TextChanged");
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return )
+            {   ///we got return, check the commend
+                ///DateTime currdt = DateTime.Now;
+                ///listBox1.Items.Add( currdt.Ticks + "-In: " + textBox1.Text );
+                ///WriteTextLine(currdt.Ticks + "-In: " + textBox1.Text);
+
+                listBox1.Items.Add("cmd: " + textBox1.Text);
+                listBox1.Items.Add("cmd: " + CmdParser(textBox1.Text));
+                textBox1.Clear();
+            }
+        }
+
+
+///-------------------------------------------------------------------------------------------------------------------------------------------------
+///
+        private void ListBoxShow(string Msg)
+        {
+            listBox1.Items.Add(Msg);
+            listBox1.Items.MoveCurrentToLast();
+            Console.WriteLine(Msg);
+        }
+
+        private string CmdParser(string Cmds)
+        {
+            ///string Ret = "Parse Done!";
+            string Ret = "unknown cmd!!";
+
+            if (Cmds == "filter")
+            {
+                string[] tmp = Cmds.Split(new char[] { ' ', '-' });
+                listBox1.Items.Add(tmp[0]);
+                Ret = "Filter Done!";
+            }
+            return Ret;
+        }
+
+
+        private string ReadText()
+        {
+            //FileStream fs = File.OpenRead("C:\Documents and Settings\richard.conway\My Documents\Visual Studio Projects\TextReader\bin\Debug\readtext.rtf")
+            FileStream f = File.OpenRead(System.Environment.CurrentDirectory + "\\mcelog.mlg");
+            StreamReader s = new StreamReader(f);
+            ///int ch = f.ReadByte();
+            return s.ReadToEnd();
+        }
+
+        private string ReadTextLine()
+        {
+            //FileStream fs = File.OpenRead("C:\Documents and Settings\richard.conway\My Documents\Visual Studio Projects\TextReader\bin\Debug\readtext.rtf")
+            FileStream f = File.OpenRead(System.Environment.CurrentDirectory + "\\mcelog.mlg");
+            StreamReader s = new StreamReader(f);
+            string r = s.ReadLine();
+            f.Close();
+            return r;
+        }
+
+        private string ReadTextLine(string filepath)
+        {
+            FileStream f = File.OpenRead(System.Environment.CurrentDirectory + filepath);
+            StreamReader s = new StreamReader(f);
+            string r = s.ReadLine();
+            r = s.ReadLine();
+            r = s.ReadLine();
+            f.Close();
+            return r;
+        }
+
+        private void WriteTextLine( string wIn )
+        {
+            //FileStream fs = File.OpenRead("C:\Documents and Settings\richard.conway\My Documents\Visual Studio Projects\TextReader\bin\Debug\readtext.rtf")
+            FileStream f = File.OpenWrite(System.Environment.CurrentDirectory + "\\mcelog.mlg");
+            f.Position = f.Length;
+            StreamWriter w = new StreamWriter(f);
+            w.WriteLine("");
+            w.Flush();
+            w.Write(wIn);
+            w.Flush();
+            f.Close();
+        }
+
+        private void mailSent()
+        {
+            MailMessage mail = new MailMessage();
+            ///NetworkCredential cred = new NetworkCredential("spanliu@gmail.com", "gmail999", "gmail");
+            //收件者
+            mail.To.Add("spanliu@hotmail.com");
+            mail.Subject = "subject";
+            //寄件者
+            mail.From = new System.Net.Mail.MailAddress("spanliu@gmail.com");
+            mail.IsBodyHtml = true;
+            mail.Body = "message";
+            //設定SMTP
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.UseDefaultCredentials = false;
+            smtp.EnableSsl = true;
+            ///smtp.Credentials = cred;
+            smtp.Port = 587;
+            //送出Mail
+            smtp.Send(mail);
+
+        }
+
+///-------------------------------------------------------------------------------------------------------------------------------------------------
+///
+        private LinkedList<string> myInC = new LinkedList<string>();
+        private LinkedList<string> myExC = new LinkedList<string>();
+
+        private class myFile
+        {
+            private FileStream thisFile;
+            private StreamReader thisStream;
+            private bool bFileInited;
+
+            public myFile(string filepath)
+            { 
+                thisFile = File.OpenRead(System.Environment.CurrentDirectory + filepath);
+                thisStream = new StreamReader(thisFile);
+                bFileInited = true;
+            }
+/*
+            public ~myFile()
+            {
+                if (bFileInited)
+                {
+                    thisStream.Close();
+                    thisFile.Close();
+                    bFileInited = false;
+                }
+            }
+*/
+            public bool myDestroy()
+            {
+                if (bFileInited)
+                {
+                    thisStream.Close();
+                    thisFile.Close();
+                    bFileInited = false;
+                }
+
+                return true;
+            }
+
+            public bool IsInited()
+            {
+                bool bRet = false;
+
+                if (bFileInited)
+                    bRet = true;
+
+                return bRet;
+            }
+
+            public bool IsEOF()
+            {
+                return thisStream.EndOfStream;
+            }
+
+            public string ReadNextLine()
+            {
+                string sRet = "";
+
+                if (bFileInited)
+                {
+                    sRet = thisStream.ReadLine();
+                }
+
+                return sRet;
+            }
+
+        }
+
+        public class ThreadedTcpSrvr
+        {
+            private TcpListener client;
+
+            public ThreadedTcpSrvr()
+            {
+                ///client = new TcpListener(9050);
+                client = new TcpListener(IPAddress.Any, 9050);
+                client.Start();
+
+                ///Console.WriteLine("Waiting for clients...");
+                ///listBox1.Items.Add("tcp: " + "Waiting for clients...");
+                while (true)
+                {
+                    while (!client.Pending())
+                    {
+                        Thread.Sleep(1000);
+                    }
+
+                    ConnectionThread newconnection = new ConnectionThread();
+                    newconnection.threadListener = this.client;
+                    Thread newthread = new Thread(new ThreadStart(newconnection.HandleConnection));
+                    newthread.Start();
+                }
+            }
+        }
+
+        class ConnectionThread
+        {
+            public TcpListener threadListener;
+            private static int connections = 0;
+
+            public void HandleConnection()
+            {
+                int recv;
+                byte[] data = new byte[1024];
+
+                TcpClient client = threadListener.AcceptTcpClient();
+                NetworkStream ns = client.GetStream();
+                connections++;
+                ///Console.WriteLine("New client accepted: {0} active connections", connections);
+                ///listBox1.Items.Add("tcp: " + "Waiting for clients...");
+
+                string welcome = "Welcome to my test server";
+                data = Encoding.ASCII.GetBytes(welcome);
+                ns.Write(data, 0, data.Length);
+
+                while (true)
+                {
+                    data = new byte[1024];
+                    recv = ns.Read(data, 0, data.Length);
+                    if (recv == 0)
+                        break;
+
+                    ns.Write(data, 0, recv);
+                }
+                ns.Close();
+                client.Close();
+                connections--;
+                ///Console.WriteLine("Client disconnected: {0} active connections", connections);
+            }
+        }
+
+
+
+    }///public partial class MainWindow : Window
+
+
+
+}
---
+++ VC#/WPF/WpfApp02/WpfApp02/MainWindow.xaml.cs
@@ -0,0 +1,312 @@
+﻿#define USETCPHOSTTHREAD
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Threading;     ///for thread
+using System.Diagnostics;   ///for trace
+using System.IO;            ///for TextWrite
+using System.Net;           ///for network
+using System.Net.Sockets;   ///for winsock
+
+
+
+
+namespace WpfApp02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ///set Disconnect button default OFF
+            button3.IsEnabled = false;
+            iRunType = 0;   ///0 for not run, 1 for server run, 2 for client run
+        }
+
+//-------------------------------------------------------------------------------------
+        private TcpHostThread TcpServer;
+        private TcpClientThread TcpClient;
+        private Thread newTcpHost;
+        private Thread newTcpClient;
+        private int iRunType;
+
+        private void addListBoxString(ListBox TargetListBox, string InputStr)
+        {
+            TargetListBox.Items.Add(InputStr);
+        }
+
+        public void addListBoxString(string InputStr)
+        {
+            listBox1.Items.Add(InputStr);
+        }
+
+        private bool bExitthread;
+
+        private void TcpHostMethod()
+        {
+            ///int result = 1;
+            ///Console.WriteLine(result.ToString() + " from thread ID: " + AppDomain.GetCurrentThreadId().ToString());
+            ///
+            bExitthread = false;
+            while (!bExitthread)
+            {
+                Thread.Sleep(1500);
+                ///addListBoxString("In TcpHostMethod!!");
+                Trace.WriteLine("In TcpHostMethod!!");
+                Console.WriteLine("In TcpHostMethod!!");
+
+            }
+        }
+//-------------------------------------------------------------------------------------
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {   ///Tcp Host control,
+            ///
+            if (true == button1.IsEnabled)
+            {
+                addListBoxString(listBox1, "TCP host Up");
+                button1.IsEnabled = false;
+                button2.IsEnabled = false;
+                button3.IsEnabled = true;
+#if USETCPHOSTTHREAD
+                TcpServer = new TcpHostThread();
+                newTcpHost = new Thread(new ThreadStart(TcpServer.TcpHostMethod));
+                newTcpHost.Start();
+                iRunType = 1;   ///0 for not run, 1 for server run, 2 for client run
+#else
+                newTcpHost = new Thread(new ThreadStart(this.TcpHostMethod));
+                newTcpHost.Start();
+#endif
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (true == button1.IsEnabled)
+            {
+                addListBoxString(listBox1, "TCP client Up");
+                button1.IsEnabled = false;
+                button2.IsEnabled = false;
+                button3.IsEnabled = true;
+
+                TcpClient = new TcpClientThread();
+                newTcpClient = new Thread(new ThreadStart(TcpClient.TcpClientMethod));
+                newTcpClient.Start();
+                iRunType = 2;   ///0 for not run, 1 for server run, 2 for client run
+            }
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {   ///Tcp disconnect
+            ///
+            if (true == button3.IsEnabled)
+            {
+                addListBoxString(listBox1, "TCP disconnect");
+                button1.IsEnabled = true;
+                button2.IsEnabled = true;
+                button3.IsEnabled = false;
+#if USETCPHOSTTHREAD
+                if( 1 == iRunType )
+                    TcpServer.StopThread();
+                if( 2 == iRunType )
+                    TcpClient.StopThread();
+#else
+                newTcpHost.Abort();
+#endif
+                iRunType = 0;   ///0 for not run, 1 for server run, 2 for client run
+            }
+        }
+    }   ///public partial class MainWindow : Window
+
+
+
+    public class TcpHostThread
+    {
+        private bool bExitthread;
+        private TcpListener HostListen;
+        private ConnectionThread newconnection;
+        private Thread newthread;
+        private Int32 port = 13000;
+        private IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+ 
+        public void TcpHostMethod()
+        {
+            ///int result = 1;
+            ///Console.WriteLine(result.ToString() + " from thread ID: " + AppDomain.GetCurrentThreadId().ToString());
+
+            ///HostListen = new TcpListener(9050);
+            HostListen = new TcpListener(localAddr, port);
+            HostListen.Start();
+            Console.WriteLine("TcpHostThread: In TcpHostMethod:" + localAddr.ToString() + " " + port.ToString() );
+
+            Console.WriteLine("Waiting for clients...");
+            bExitthread = false;
+            while (!bExitthread)
+            {
+                while (!HostListen.Pending() && !bExitthread)
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("TcpHostThread: In HostListen Pending!!");
+                }
+                ///Thread.Sleep(1500);
+                Console.WriteLine("TcpHostThread: In TcpHostMethod!!");
+
+                if (!bExitthread)
+                {
+                    Console.WriteLine("TcpHostThread: Client found!!");
+
+                    newconnection = new ConnectionThread();
+                    newconnection.threadListener = this.HostListen;
+                    newthread = new Thread(new ThreadStart(newconnection.HandleConnection));
+                    newthread.Start();
+                }
+
+            }
+            Console.WriteLine("TcpHostThread: Quit TcpHostMethod!!");
+            if (bExitthread)
+            {
+                Console.WriteLine("TcpHostThread: Abort newthread!!");
+                newthread.Abort();
+            }
+        }
+
+        public void StopThread()
+        {
+            bExitthread = true;
+        }
+
+/*
+        public static void Main()
+        {
+            SimpleThread simpleThread = new SimpleThread();
+            simpleThread.SimpleMethod();
+
+            ThreadStart ts = new ThreadStart(simpleThread.SimpleMethod);
+            Thread t = new Thread(ts);
+            t.Start();
+        }
+*/ 
+    }
+
+
+    public class TcpClientThread
+    {
+        ///private IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        private TcpClient server;
+        private Int32 port = 13000;
+        private bool bExitthread;
+
+        public void TcpClientMethod()
+        {
+
+            byte[] data = new byte[1024];
+            string input, stringData;
+
+            try
+            {
+                server = new TcpClient("127.0.0.1", port);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Unable to connect to server");
+                return;
+            }
+            NetworkStream ns = server.GetStream();
+
+            bExitthread = false;
+            while (!bExitthread)
+            {
+                //input = Console.ReadLine();
+                //if (input == "exit")
+                //    break;
+                input = "This client message!!";
+                ns.Write(Encoding.ASCII.GetBytes(input), 0, input.Length);
+                ns.Flush();
+
+                data = new byte[1024];
+                int recv = ns.Read(data, 0, data.Length);
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine("Msg from server: " + stringData);
+
+                Thread.Sleep(1900);
+            }
+            Console.WriteLine("Disconnecting from server...");
+            ns.Close();
+            server.Close();
+        }
+
+        public void StopThread()
+        {
+            bExitthread = true;
+        }
+    }
+
+
+    public class ConnectionThread
+    {
+        public TcpListener threadListener;
+        private static int connections = 0;
+
+        public void HandleConnection()
+        {
+            int recv;
+            byte[] data = new byte[1024];
+            string stringData;
+
+            TcpClient client = threadListener.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+            connections++;
+            Console.WriteLine("New client accepted: {0} active connections", connections);
+
+            string welcome = "Welcome to my test server";
+            data = Encoding.ASCII.GetBytes(welcome);
+            ns.Write(data, 0, data.Length);
+
+            while (true)
+            {
+                data = new byte[1024];
+#if true
+                try
+                {
+                    recv = ns.Read(data, 0, data.Length);
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Unable to read from client");
+                    recv = 0;
+                }
+#else
+                recv = ns.Read(data, 0, data.Length);
+#endif
+                if (recv == 0)
+                    break;
+
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine("Msg from clinet: " + stringData);
+                ns.Write(data, 0, recv);
+            }
+            ns.Close();
+            client.Close();
+            connections--;
+            Console.WriteLine("Client disconnected: {0} active connections", connections);
+        }
+    }
+
+
+}
